25784d4c27e9556c01278022fa8987f53112897b
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:31:44 +0200

    Update

--- source/apps/inventories/package.d
+++ source/apps/inventories/package.d
@@ -2,13 +2,33 @@ module apps.inventories;
 
 mixin(ImportPhobos!());
 
-public { // required uim libraries
-  import uim.core;
-  import uim.oop;
-  import uim.apps;
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.inv.controllers;
+  import apps.inv.helpers;
+  import apps.inv.tests;
+  import apps.inv.views;
 }
 
-public { // admin-inventories libraries
-  import apps.inventories.controllers;
-  import apps.inventories.views;
+DApp invApp;
+static this() {
+  invApp = App
+    .name("inventoriesApp")
+    .rootPath("/apps/inventories")
+    .addRoute(Route("", HTTPMethod.GET, INVIndexPageController))
+    .addRoute(Route("/", HTTPMethod.GET, INVIndexPageController));
 }
